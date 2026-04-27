130e7bc081f3834716b834bf924ca864c7de0881
CarlosSalas1325 <luissalasjh13@gmail.com>
2026-04-27 17:37:43 -0600

    debug: add boot timing logs

--- api/index.js
+++ api/index.js
@@ -15,13 +15,20 @@ let handler;
 
 module.exports = async (req, res) => {
   if (!handler) {
+    const t0 = Date.now();
+    console.log('[boot] start');
+
     const { NestFactory } = require('@nestjs/core');
+    console.log('[boot] NestFactory loaded', Date.now() - t0, 'ms');
+
     const { ValidationPipe } = require('@nestjs/common');
     const { AppModule } = require(path.join(__dirname, '..', 'dist', 'src', 'app.module'));
+    console.log('[boot] AppModule loaded', Date.now() - t0, 'ms');
 
     const app = await NestFactory.create(AppModule, {
       logger: ['error', 'warn'],
     });
+    console.log('[boot] NestFactory.create done', Date.now() - t0, 'ms');
 
     app.enableCors({
       origin: process.env.FRONTEND_URL || '*',
@@ -37,6 +44,8 @@ module.exports = async (req, res) => {
     );
 
     await app.init();
+    console.log('[boot] app.init done', Date.now() - t0, 'ms');
+
     handler = serverlessHttp(app.getHttpAdapter().getInstance());
   }
 
