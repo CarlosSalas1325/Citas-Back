4bfca793291f5fd6ccdd8aedbe277878c6b3f9ae
CarlosSalas1325 <luissalasjh13@gmail.com>
2026-04-27 16:38:42 -0600

    fix: replace bcrypt with bcryptjs for serverless compatibility

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { randomInt } from 'crypto';
 import { DatabaseService } from '../../database/database.service';
 import { RegisterDto, LoginDto, VerifyOtpDto } from './dto/auth.dto';
